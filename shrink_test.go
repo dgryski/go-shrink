264401cadb7ccf5616db9b83e9aae1dbf6b21d7d
Damian Gryski <dgryski@gmail.com>
2015-10-31 09:13:27 +0100

    make listshrinker generic via reflection
    Fixes #1

--- shrink_test.go
+++ shrink_test.go
@@ -7,68 +7,106 @@ import (
 	"testing/quick"
 )
 
-func intlistshrinker(v reflect.Value, tactic int) (rv reflect.Value, err error) {
-
-	var nv []int
-	iv := v.Interface().([]int)
+func listshrinker(v reflect.Value, tactic int) (rv reflect.Value, err error) {
 
 	switch tactic {
 	case 0:
-		if len(iv) < 2 {
+		if v.Len() < 2 {
 			return reflect.Value{}, ErrDeadEnd
 		}
 
-		nv = make([]int, len(iv)/2)
-		copy(nv, iv)
-		return reflect.ValueOf(nv), nil
+		nv := reflect.MakeSlice(v.Type(), v.Len()/2, v.Len()/2)
+		reflect.Copy(nv, v)
+		return nv, nil
 	case 1:
-
-		if len(iv) < 2 {
+		if v.Len() < 2 {
 			return reflect.Value{}, ErrDeadEnd
 		}
 
-		nv = make([]int, len(iv)/2)
-		copy(nv, iv[len(nv):])
-		return reflect.ValueOf(nv), nil
+		nv := reflect.MakeSlice(v.Type(), v.Len()/2, v.Len()/2)
+		reflect.Copy(nv, v.Slice(v.Len()/2, v.Len()))
+		return nv, nil
 	case 2:
-
-		if len(iv) < 2 {
+		if v.Len() < 2 {
 			return reflect.Value{}, ErrDeadEnd
 		}
 
-		nv = make([]int, len(iv)-1)
-		copy(nv, iv)
-		return reflect.ValueOf(nv), nil
+		nv := reflect.MakeSlice(v.Type(), v.Len()-1, v.Len()-1)
+		reflect.Copy(nv, v)
+		return nv, nil
 	case 3:
-
-		if len(iv) < 2 {
+		if v.Len() < 2 {
 			return reflect.Value{}, ErrDeadEnd
 		}
 
-		nv = make([]int, len(iv)-1)
-		copy(nv, iv[1:])
-		return reflect.ValueOf(nv), nil
+		nv := reflect.MakeSlice(v.Type(), v.Len()-1, v.Len()-1)
+		reflect.Copy(nv, v.Slice(1, v.Len()))
+		return nv, nil
 	case 4:
+		if v.Len() < 2 {
+			return reflect.Value{}, ErrDeadEnd
+		}
 
-		if len(iv) < 2 {
+		if !isNumeric(v.Type().Elem()) {
 			return reflect.Value{}, ErrDeadEnd
 		}
 
 		var allzeros bool
 
-		nv = make([]int, len(iv))
-		copy(nv, iv)
-		for i, nn := range nv {
-			allzeros = allzeros || nn == 0
-			nv[i] = nn / 2
+		nv := reflect.MakeSlice(v.Type(), v.Len(), v.Len())
+		reflect.Copy(nv, v)
+
+		for i := 0; i < nv.Len(); i++ {
+			nn := nv.Index(i)
+			allzeros = allzeros || isZero(nn)
+			div2(nn)
 		}
 		if allzeros {
 			return reflect.Value{}, ErrDeadEnd
 		}
-		return reflect.ValueOf(nv), nil
+		return nv, nil
 	}
 
 	return reflect.Value{}, ErrNoMoreTactics
+}
+
+func isNumeric(v reflect.Type) bool {
+	switch v.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64,
+		reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64,
+		reflect.Float32, reflect.Float64:
+		return true
+	}
+
+	return false
+}
+
+func isZero(v reflect.Value) bool {
+
+	switch v.Type().Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	}
+
+	panic("isZero: not a numeric type")
+}
+
+func div2(v reflect.Value) {
+
+	switch v.Type().Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		v.SetInt(v.Int() / 2)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		v.SetUint(v.Uint() / 2)
+	case reflect.Float32, reflect.Float64:
+		v.SetFloat(v.Float() / 2)
+	default:
+		panic("div2: not a numeric type")
+	}
 }
 
 type smallints []int
@@ -108,7 +146,7 @@ func TestQuick(t *testing.T) {
 	if err := quick.Check(checkReverse, nil); err != nil {
 		t.Error(err)
 		check := err.(*quick.CheckError)
-		s, serr := Shrink(checkReverse, check.In, []Shrinker{intlistshrinker})
+		s, serr := Shrink(checkReverse, check.In, []Shrinker{listshrinker})
 		t.Logf("shrunk: %v serr=%+v", s[0].Interface(), serr)
 	}
 }
